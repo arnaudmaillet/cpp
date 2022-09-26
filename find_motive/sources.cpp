06968b1cfce51728baea0a2fe17845029af0ce5e
Arnaud <arnaud97.nono@gmail.com>
2022-09-26 19:24:19 +0200

    find motive works

--- find_motive/sources.cpp
+++ find_motive/sources.cpp
@@ -1,5 +1,4 @@
 #include <fstream>
-#include <string.h>
 #include "sources.h"
 
 int motive(char *path, char *motive)
@@ -13,23 +12,14 @@ int motive(char *path, char *motive)
         return 1;
     }
     std::string str;
-    file >> str;
-    int len = str.size();
-    char buff[len + 1];
     int count = 0;
-    
 
-    strcpy(buff, str.c_str());
-    char *word = strtok(buff, " ");
-    while (word != NULL)
+    while (file >> str)
     {
-        std::string s(word);
-        std::cout << s;
-        if (s.find(motive) != -1)
+        if (str.find(motive) != -1)
         {
             count++;
         }
-        word = strtok(NULL, " ");
     }
     std::cout << "The file " << path << " contains " << count << " words containing the motive " << motive << "\n";
     return 0;
